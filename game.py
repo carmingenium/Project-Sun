84221f199538a34160a9a121be90feca6d9e3dd7
Dağhan Sever <daghansever@gmail.com>
2025-11-17 16:34:57 +0300

    logic cleared out
    every file is focusing on its own functions now, no combined logic

--- game.py
+++ game.py
@@ -1,12 +1,31 @@
+import pygame
 import character
-import skill
+import renderer
 
-def initialize_characters():
+print("Imported:", renderer)
+print("Path:", renderer.__file__)
+print("Members:", dir(renderer))
+
+
+
+#region Variables
+# list of all encounters & characters
+characterslist = []
+spriteslist = []
+# party
+party_list = []
+#endregion
+
+def initializeGame():
+  # initialize story text
+  with open("story/test.txt", "r", encoding="utf-8") as f: # load text
+    dialog_lines = [line.strip() for line in f.readlines()]
+  # font = pygame.font.Font(None, 32)   # None = default font, 32 = size
+  # initialize characters
+  initializeCharacters()
+def initializeCharacters():
   #region charlist
-  # list of all encounters & characters
-  characterslist = []
-  # party
-  party_list = []
+
   # player = character.Character(
   #   name="Blacked out Engineer",
   #   speed=(5, 10),
@@ -19,48 +38,75 @@ def initialize_characters():
   # clown
   # mime
   # unknown
-  
-  
+
+
   # encounter 1
   # X amount space carps
-  
-  
-  # encounter 2 
+
+
+  # encounter 2
   # X amount zombies
-  
-  
-  # encounter 3 
+
+
+  # encounter 3
   # Changeling
   # Traitor
-  
-  
-  # encounter 4 
+
+
+  # encounter 4
   # Moon Heretic (alternative ending)
-  
+
   # encounter 5 (boss fight)
   # 3 way fight (3 endings)
-  
+
   # Party 1
   # Space Dragon
-  
+
   # Party 2
   # Nukie Gunner (dps, cc)
   # Nukie Engineer (summoner, heals borg)
   # Nukie Chemist (dps, healer, debuff)
   # Nukie Borg  (tank dps)
   #endregion
-  
-  
-  
-  
-  
-  
+  unknown = character.Character(
+    name="Unknown",
+    sprite='sprites/unknown.png',
+    speed=(3, 7),
+    hp=100,
+    skills=[],
+    supportSkills=[]
+  )
+  characterslist.append(unknown)
+
+  spacedragon = character.Character(
+    name="Space Dragon",
+    sprite='sprites/spacedragon.png',
+    speed=(4, 8),
+    hp=150,
+    skills=[],
+    supportSkills=[]
+  )
+  characterslist.append(spacedragon)
+
+
+
+  renderer.spriteListInitialize(characterslist)
   return
-
-
+def getCharacterList():
+  return characterslist
 
 
 def main():
+  initializeGame()
+  running = True
+  # renderer.renderNovelScene(0, "This is a test dialogue line.") # have to send index for now as there are 2 lists in two different files.
+  renderer.renderCombatScene()
+  while running:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        running = False
+
+  # clock.tick(60)  # Limit to 60 frames per second
   return
 
-
+main()
--- renderer.py
+++ renderer.py
@@ -2,21 +2,26 @@ import pygame
 # initialization
 pygame.init()
 screen = pygame.display.set_mode((1920, 1080)) # this has to be static as long as the camera system depends on it
-# game init
-with open("story/test.txt", "r", encoding="utf-8") as f: # load text
-  dialog_lines = [line.strip() for line in f.readlines()]
-font = pygame.font.Font(None, 32)   # None = default font, 32 = size
 
 
 
-current_line_index = 0
 
 # sprite loading
 dragon_sprite = pygame.image.load('sprites/spacedragon.png').convert() # 64x64 so double of a normal human
 dragon_sprite_big = pygame.transform.scale(dragon_sprite, (dragon_sprite.get_width()*2, dragon_sprite.get_height()*2))# 128x128
-
 unknown_sprite = pygame.image.load('sprites/unknown.png').convert() # 32x32
 
+
+sprites = []
+characterList = []
+def spriteListInitialize(characters):
+  for char in characters:
+    try:
+      sprites.append(pygame.image.load(char.sprite).convert())
+      print(f"Loaded sprite for {char.name} from path {char.sprite}")
+    except: 
+      print(f"Error loading sprite for {char.name} from path {char.sprite}") 
+  characterList = characters
 
 
 # vars
@@ -26,62 +31,84 @@ center_x = 960
 center_y = 540
 clock = pygame.time.Clock() # frame rate controller
 
+# for dialogues
+renderposRight = (1400, 200)
+renderposLeft = (100, 200)
 
 
 
+
+
+#region rendering functions
+
+#region NOTES
+# positional hypothesis:
+# to put it in the middle in y axis: 540 - (sprite_height / 2)
+# to put it in the middle in x axis: 960 - (sprite_width / 2)
+# normal characters are 32x32, might get scaled
+# the position sprites are drawn are from top-left of the sprites. so x can be closer to 0 and y needs to be further down than usual
+# testing values
+#endregion
 
 def spriteNovelify(sprite):
   novelSprite = pygame.transform.scale(sprite, (sprite.get_width()*35, sprite.get_height()*35))
   return novelSprite
 
+def findCharacterSprite(character):
+  for char in characterList:
+    if char.name == character.name:
+      index = characterList.index(char)
+      return sprites[index]
+
+def renderNovelScene(character, dialogue_line):
+  # start drawing
+  # find speaking character
+  # load sprite in novel format
+  
+  # possibly putting main character on left and others on right
+  # if(character.name == "Blacked out Engineer"): renderpos = left
+  # else: renderpos = right
+  
+  sprite = findCharacterSprite(character)
+  novelSprite = spriteNovelify(sprite)
+  screen.blit(novelSprite, (center_x - (novelSprite.get_width() // 2), center_y - (novelSprite.get_height() // 2)))
+  # find right textlist
+  # load text box
+  novelText = pygame.Surface((1920,200), pygame.SRCALPHA)
+  novelText.fill((0, 0, 30, 150))  # semi-transparent black
+  
+  font = pygame.font.Font(None, 32)   # None = default font, 32 = size
+  dialogue = font.render(dialogue_line, True, (255, 255, 255))  # white text
+  novelText.blit(dialogue, (20, 20))  # small padding from left and top
+  
+  screen.blit(novelText, (0, 880))  # position at bottom of screen
+  
+  
+  pygame.display.flip()  # Update the display to show changes
+  
+def renderCombatScene(playerParty=None, enemyParty=None):
+  # testing render
+  screen.blit(dragon_sprite, (100, dragoncenter_y - 128)) 
+  screen.blit(dragon_sprite, (300, dragoncenter_y - 128))  # upper middle
+  screen.blit(dragon_sprite, (500, dragoncenter_y - 128))
+  
+  screen.blit(dragon_sprite, (100, dragoncenter_y + 128)) 
+  screen.blit(dragon_sprite, (300, dragoncenter_y + 128))  # upper middle
+  screen.blit(dragon_sprite, (500, dragoncenter_y + 128))
+  
+  for char in playerParty or []:
+    sprite = findCharacterSprite(char)
+    screen.blit(sprite, (100 , center_y)) # instead of 100 need a math function to calculate positions based on party size
+    
+  for char in enemyParty or []:
+    sprite = findCharacterSprite(char)
+    screen.blit(sprite, (1600 , center_y)) # instead of 1600 need a math function to calculate positions based on party size
+    
+    
+  pygame.display.flip()  # Update the display to show changes
+#endregion
+
 def main():
-  current_gamemode = "novel" # VN mode or combat mode
-  running = True
-
-  while running:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        running = False
-    screen.fill((0, 0, 0))  # Clear screen with black
-    if(current_gamemode == "novel"):
-      # start drawing
-      # find speaking character
-      # load sprite in novel format
-      novelSprite = spriteNovelify(dragon_sprite)
-      screen.blit(novelSprite, (center_x - (novelSprite.get_width() // 2), center_y - (novelSprite.get_height() // 2)))
-      # find right textlist
-      # load text box
-      novelText = pygame.Surface((1920,200), pygame.SRCALPHA)
-      novelText.fill((0, 0, 0, 150))  # semi-transparent black
-      
-      dialogue = font.render(dialog_lines[0], True, (255, 255, 255))
-      novelText.blit(dialogue, (20, 20))  # small padding from left and top
-      
-      screen.blit(novelText, (0, 880))  # position at bottom of
-    
-    
-    if(current_gamemode == "combat"):
-    # start drawing
-    # positional hypothesis:
-    # to put it in the middle in y axis: 540 - (sprite_height / 2)
-    # to put it in the middle in x axis: 960 - (sprite_width / 2)
-    # normal characters are 32x32, might get scaled
-    
-    # the position sprites are drawn are from top-left of the sprites. so x can be closer to 0 and y needs to be further down than usual
-    
-    # testing values
-      screen.blit(dragon_sprite, (100, dragoncenter_y - 128)) 
-      screen.blit(dragon_sprite, (300, dragoncenter_y - 128))  # upper middle
-      screen.blit(dragon_sprite, (500, dragoncenter_y - 128))
-      
-      screen.blit(dragon_sprite, (100, dragoncenter_y + 128)) 
-      screen.blit(dragon_sprite, (300, dragoncenter_y + 128))  # upper middle
-      screen.blit(dragon_sprite, (500, dragoncenter_y + 128))
-    pygame.display.flip()  # Update the display to show changes
-    
-    clock.tick(60)  # Limit to 60 frames per second
-  pygame.quit()
-
   return
 
 main()
